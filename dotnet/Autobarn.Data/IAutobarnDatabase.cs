9a2d21ae5c9d428f37ae2537e5aaa62982aefe70
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-19 22:34:16 +0100

    Starting point for .NET workshop

---
+++ dotnet/Autobarn.Data/IAutobarnDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autobarn.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autobarn.Data {
+	public interface IAutobarnDatabase {
+		public IEnumerable<Vehicle> ListVehicles();
+		public IEnumerable<Manufacturer> ListManufacturers();
+		public IEnumerable<Model> ListModels();
+
+		public Vehicle FindVehicle(string registration);
+		public Model FindModel(string code);
+		public Manufacturer FindManufacturer(string code);
+
+		public void CreateVehicle(Vehicle vehicle);
+		public void UpdateVehicle(Vehicle vehicle);
+		public void DeleteVehicle(Vehicle vehicle);
+	}
+}

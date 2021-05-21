ffd56b6021bcce80b29160482d75ce21297cea8e
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-21 13:03:19 +0100

    Working gRPC demo

---
+++ grpc/GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient {
+	class Program {
+		private const string GRPC_URL = "https://localhost:5001";
+		static void Main(string[] args) {
+            using var channel = GrpcChannel.ForAddress(GRPC_URL);
+            var greeter = new Greeter.GreeterClient(channel);
+            while (true) {
+	            var request = new HelloRequest() {Name = "Everybody!"};
+	            var reply = greeter.SayHello(request);
+	            Console.WriteLine(reply.Message);
+	            Console.ReadKey(false);
+            }
+		}
+	}
+}

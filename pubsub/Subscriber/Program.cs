0a9759a0ef106702fc6ed1fdaabd305000039bfe
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-21 11:35:24 +0100

    Pubsub demo with publisher and subscriber working

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 
-namespace Subscriber
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Subscriber {
+	class Program {
+		const string BUS_CONNECTION_STRING = "Endpoint=sb://pubsub-demo-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fvr0Z5xbCwACLnRGmTWj6XZTd6y4wgjC4xEtPycXUkc=";
+        const string TOPIC = "pubsub-demo-topic";
+        const string SUBSCRIPTION = "pubsub-demo-subscription-1";
+		static async Task Main(string[] args) {
+			var client = new ServiceBusClient(BUS_CONNECTION_STRING);
+            var processor = client.CreateProcessor(TOPIC, SUBSCRIPTION, new ServiceBusProcessorOptions());
+            processor.ProcessMessageAsync += ProcessMessage;
+            processor.ProcessErrorAsync += _ => Task.CompletedTask;
+            Console.WriteLine("Waiting for messages. Press any key to exit.");
+            await processor.StartProcessingAsync();
+            Console.ReadKey(false);
+            await processor.StopProcessingAsync();
+		}
+
+        private static async Task ProcessMessage(ProcessMessageEventArgs args) {
+            Console.WriteLine(args.Message.Body);
+            await args.CompleteMessageAsync(args.Message);
         }
-    }
+	}
 }
